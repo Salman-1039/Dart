ed04f720ddc0959461ec41407cfcfd337b7341a5
Muhammad Salman Tariq <75311012+Salman-1039@users.noreply.github.com>
2024-12-11 14:31:24 +0500

    Delete My-Theme directory

--- My-Theme/404.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<main id="main-content">
-    <h1>Page Not Found</h1>
-    <p>Sorry, the page you're looking for doesn't exist. Try searching or navigate back to the homepage.</p>
-    <?php get_search_form(); ?>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
